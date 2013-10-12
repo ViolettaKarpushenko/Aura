cb20358c023bc694088967dbf25d836e853e9ed7
Pavel  Zaitsau <pavel.zaitsau@hotmail.com>
2013-10-13 02:23:25 +0300

    Small cleanup

--- Aura.Web/Data/AnimalRepository.cs
+++ Aura.Web/Data/AnimalRepository.cs
@@ -81,93 +81,67 @@ namespace Aura.Web.Data
             var stocks = GetStocks().Items.AsParallel();
 
             var avgLos = stocks.Average(stock => stock.Los);
-
             var avgOlen = stocks.Average(stock => stock.Olen);
-
             var avgKosula = stocks.Average(stock => stock.Kosula);
-
             var avgKaban = stocks.Average(stock => stock.Kaban);
-
             var avgZayacBelak = stocks.Average(stock => stock.ZayacBelak);
-
             var avgZayacRusak = stocks.Average(stock => stock.ZayacRusak);
-
             var avgKunica = stocks.Average(stock => stock.Kunica);
-
             var avgLisica = stocks.Average(stock => stock.Lisica);
-
             var avgOndatra = stocks.Average(stock => stock.Ondatra);
-
             var avgNorka = stocks.Average(stock => stock.Norka);
-
             var avgBobr = stocks.Average(stock => stock.Bobr);
-
             var avgVolk = stocks.Average(stock => stock.Volk);
-
             var avgBarsuk = stocks.Average(stock => stock.Barsuk);
-
             var avgVydra = stocks.Average(stock => stock.Vydra);
-
             var avgEnotovidnayaSobaka = stocks.Average(stock => stock.EnotovidnayaSobaka);
-
             var avgRys = stocks.Average(stock => stock.Rys);
-
             var avgBelka = stocks.Average(stock => stock.Belka);
-
             var avgGluhar = stocks.Average(stock => stock.Gluhar);
-
             var avgTeterev = stocks.Average(stock => stock.Teterev);
-
             var avgRabchik = stocks.Average(stock => stock.Rabchik);
-
             var avgSerayaKuropatka = stocks.Average(stock => stock.SerayaKuropatka);
-
             var avgDikayaUtka = stocks.Average(stock => stock.DikayaUtka);
-
             var avgZooplankton = stocks.Average(stock => stock.Zooplankton);
-
             var avgBentos = stocks.Average(stock => stock.Bentos);
-
             var avgRyba = stocks.Average(stock => stock.Ryba);
-
-            var avgPtica = stocks.Average(stock2 =>
-                            stock2.DikayaUtka / avgDikayaUtka +
-                            stock2.SerayaKuropatka / avgSerayaKuropatka +
-                            stock2.Rabchik / avgRabchik +
-                            stock2.Teterev / avgTeterev +
-                            stock2.Gluhar / avgGluhar);
-
-            var avgJivotnye = stocks.Average(stock2 =>
-                            stock2.ZayacBelak / avgZayacBelak +
-                            stock2.ZayacRusak / avgZayacRusak +
-                            stock2.Kunica / avgKunica +
-                            stock2.Lisica / avgLisica +
-                            stock2.Ondatra / avgOndatra +
-                            stock2.Norka / avgNorka +
-                            stock2.Bobr / avgBobr +
-                            stock2.Volk / avgVolk +
-                            stock2.Barsuk / avgBarsuk +
-                            stock2.Vydra / avgVydra +
-                            stock2.EnotovidnayaSobaka / avgEnotovidnayaSobaka +
-                            stock2.Rys / avgRys +
-                            stock2.Belka / avgBelka);
-
-            var avgKopytnye = stocks.Average(stock3 =>
-                            stock3.Los / avgLos +
-                            stock3.Olen / avgOlen +
-                            stock3.Kosula / avgKosula +
-                            stock3.Kaban / avgKaban);
 
             var results = stocks.Select(stock => new
                 {
                     stock,
-                    zaposyOzer = stock.Bentos / avgBentos + stock.Zooplankton / avgZooplankton + stock.Ryba / avgRyba
+                    zaposyOzer =    stock.Bentos            / avgBentos +
+                                    stock.Zooplankton       / avgZooplankton +
+                                    stock.Ryba              / avgRyba
                 })
                 .Select(stock => new
                 {
                     stock.stock,
                     stock.zaposyOzer,
-                    zapasyPhg = stock.zaposyOzer + avgPtica + avgJivotnye + avgKopytnye
+                    zapasyPhg = stock.zaposyOzer +
+                           (stock.stock.DikayaUtka          / avgDikayaUtka +
+                            stock.stock.SerayaKuropatka     / avgSerayaKuropatka +
+                            stock.stock.Rabchik             / avgRabchik +
+                            stock.stock.Teterev             / avgTeterev +
+                            stock.stock.Gluhar              / avgGluhar) / 4 +
+
+                           (stock.stock.ZayacBelak          / avgZayacBelak +
+                            stock.stock.ZayacRusak          / avgZayacRusak +
+                            stock.stock.Kunica              / avgKunica +
+                            stock.stock.Lisica              / avgLisica +
+                            stock.stock.Ondatra             / avgOndatra +
+                            stock.stock.Norka               / avgNorka +
+                            stock.stock.Bobr                / avgBobr +
+                            stock.stock.Volk                / avgVolk +
+                            stock.stock.Barsuk              / avgBarsuk +
+                            stock.stock.Vydra               / avgVydra +
+                            stock.stock.EnotovidnayaSobaka  / avgEnotovidnayaSobaka +
+                            stock.stock.Rys                 / avgRys +
+                            stock.stock.Belka               / avgBelka) / 13 +
+
+                           (stock.stock.Los                 / avgLos +
+                            stock.stock.Olen                / avgOlen +
+                            stock.stock.Kosula              / avgKosula +
+                            stock.stock.Kaban               / avgKaban) / 4
                 })
                 .Select(stock => new ResultModel
                 {
