6589997042ab3552cb84e9bdb62e0b8a8fec0e8b
Pavel Zaitsau <p.zaitsau@investars.com>
2013-10-27 17:56:43 +0300

    Hydrobiological assessment view, hydrochemical assessment view and part of geochemicalassessment view.

--- Aura.Web/Scripts/Views/view.ecologicalassessment.js
+++ Aura.Web/Scripts/Views/view.ecologicalassessment.js
@@ -1,21 +1,28 @@
 ﻿var EcologicalAssessmentView = Backbone.View.extend({
-    el: "#view-ecological-assessment",
-
     events: {
-        "click #element-calculator>.placeholder .item .close": "removeItem",
-        "change .item>input[type=number]": "calculate",
-        "click #clear-btn": "clear"
+        "click .item .close": "removeItem",
+        "change .item input[type=number]": "changeValculate",
+        "click .btn-clear": "clear"
     },
 
     initialize: function () {
-        $("#element-container>.placeholder>.item", this.el).draggable({
+        console.log('initialize');
+        var draggableItems = $(".element-container-placeholder .item", this.el);
+        draggableItems.draggable({
             appendTo: "body",
             helper: "clone"
         });
 
-        var calculatorContainer = $('#element-calculator>.placeholder', this.el);
+        var calculateMethod = $(this.el).attr('calculate');
+        if (calculateMethod === 'simple') {
+            this.calculate = this.simpleCalculate;
+        } else if (calculateMethod === 'extend') {
+            this.calculate = this.extendCalculate;
+        }
+
+        var calculatorContainer = $('.element-calculator-placeholder', this.el);
         calculatorContainer.droppable({
-            accept: "#element-container>.placeholder>.item",
+            accept: draggableItems,//".element-container-placeholder .item",
             drop: $.proxy(function (event, ui) {
                 var item = ui.draggable.outerHTML();
 
@@ -25,26 +32,47 @@
                     .appendTo(calculatorContainer)
                     .fadeIn();
 
+                // ReSharper disable MisuseOfOwnerFunctionThis
                 this.calculate();
+                // ReSharper restore MisuseOfOwnerFunctionThis
             }, this)
         });
     },
 
-    calculate: function () {
+    changeValculate: function () {
+        this.calculate();
+    },
+
+    simpleCalculate: function () {
         var sum = 0;
-        var items = $('#element-calculator>.placeholder>.item', this.el);
+        var items = $('.element-calculator-placeholder .item', this.el);
         items.each(function () {
             var $this = $(this);
             sum = sum + ($this.find('input[type=number]').val() | 0) / ($this.find('.value').text() | 0);
         });
 
         sum = sum / items.length;
-        $('#index-label', this.el).text(sum);
+        $('.element-calculator .index-label', this.el).text(sum);
+    },
+
+    extendCalculate: function () {
+        var sum1 = 0;
+        var sum2 = 0;
+        
+        var items = $('.element-calculator-placeholder .item', this.el);
+        items.each(function () {
+            var $this = $(this);
+            sum1 = sum1 + ($this.find('input[type=number]').val() | 0) * ($this.find('.value').text() | 0);
+            sum2 = sum2 + ($this.find('input[type=number]').val() | 0);
+        });
+
+        var result = sum1 / sum2;
+        $('.element-calculator .index-label', this.el).text(result);
     },
 
     clear: function () {
-        $('#element-calculator>.placeholder>.item', this.el).remove();
-        $('#index-label', this.el).text(0);
+        $('.element-calculator-placeholder .item', this.el).remove();
+        $('.element-calculator .index-label', this.el).text(0);
     },
 
     removeItem: function (event) {
